532709861144c519cdd6f08637158b385ccebcce
GEUNHYEONG <gim35182@g.yju.ac.kr>
2026-05-12 23:17:13 +0900

    이모티콘수정

--- config.example.js
+++ config.example.js
@@ -1,7 +1,7 @@
 window.APP_CONFIG = {
   API_KEY: "***REMOVED***",
   CALENDAR_MAP: {
-    "434a85c7be660f180486d88d6362ff5468fe73ec5a3da754eff72fa0592506c8@group.calendar.google.com@groump.calendar.google.co": "301",
-    "6be7b63f943dcc68ec839197f0a9b5a4d378637dc5700bd579ec5e03dae2cb9a@group.calendar.google.com.google.com": "405",
-  },
+    "434a85c7be660f180486d88d6362ff5468fe73ec5a3da754eff72fa0592506c8@group.calendar.google.com": "301",
+    "6be7b63f943dcc68ec839197f0a9b5a4d378637dc5700bd579ec5e03dae2cb9a@group.calendar.google.com": "405"
+  }
 };
